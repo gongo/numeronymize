9c9ef2480676c4e7cecb00099b9b9ea96219a4db
Wataru MIYAGUNI <gonngo@gmail.com>
2024-06-15 22:50:01 +0900

    :lipstick: Update component to SmartHR UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { createTheme, ThemeProvider, Button } from 'smarthr-ui'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+const theme = createTheme()
+
+const App: React.FC = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,9 +20,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </ThemeProvider>
   )
 }
 
