641f53d0a23849f7be49a860869652c171e1d9ad
Wataru MIYAGUNI <gonngo@gmail.com>
2024-06-17 22:25:20 +0900

    :sparkles: numeronymize!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
-import { useState } from 'react'
-import { Button } from 'smarthr-ui'
+import { ReactNode } from 'react'
+import { useForm, useWatch, Control } from 'react-hook-form'
+import { defaultSpacing, Base, Center, FormControl, Heading, Input, defaultBreakpoint } from 'smarthr-ui'
+import styled from 'styled-components'
+
+import { numeronymize } from './func'
+
+interface FormInputs {
+  word: string
+}
+
+const ResultForm: React.FC<{ control: Control<FormInputs> }> = ({ control }): ReactNode => {
+  const word = useWatch({
+    control,
+    name: 'word',
+  })
+
+  return <div>{numeronymize(word)}</div>
+}
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    control,
+    formState: { errors },
+  } = useForm<FormInputs>({ defaultValues: { word: '' }, mode: 'onChange' })
 
-  return <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+  return (
+    <Center>
+      <Heading type="screenTitle">Numeronym Generator</Heading>
+      <StyledBase padding="S">
+        <FormControl
+          title="Enter the word you want to represent with a numeronym."
+          helpMessage="alphanumeric characters and spaces only"
+          errorMessages={errors.word && 'Non-alphanumeric characters have been entered.'}
+        >
+          <Input
+            type="text"
+            {...register('word', {
+              pattern: /^[a-zA-Z0-9\s]*$/,
+            })}
+          />
+        </FormControl>
+      </StyledBase>
+      <StyledBase padding="S">{!errors.word && <ResultForm control={control} />}</StyledBase>
+    </Center>
+  )
 }
 
 export default App
+
+const StyledBase = styled(Base)`
+  margin-top: ${defaultSpacing.S};
+  width: 600px;
+
+  @media screen and (max-width: ${defaultBreakpoint.SP}px) {
+    width: auto;
+  }
+`
---
+++ src/func.ts
@@ -0,0 +1,13 @@
+export const numeronymize = (raw_word: string): string => {
+  const word = raw_word.replace(/\s/g, '').toLowerCase()
+
+  if (word.length <= 3) {
+    return raw_word
+  }
+
+  const firstChar = word[0]
+  const lastChar = word[word.length - 1]
+  const len = word.length - 2
+
+  return `${firstChar}${len}${lastChar}`
+}
